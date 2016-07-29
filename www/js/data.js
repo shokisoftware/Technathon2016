b0cf0924922bd052f7e17cbab213728df6161bd0
shokisoftware <shokisoftware@gmail.com>
2016-07-29 11:46:21 +0000

    Add files via upload

--- www/js/data.js
+++ www/js/data.js
@@ -1,4 +1,44 @@
 /**
  * Created by NGTKonyDevUser on 7/28/2016.
  */
-var UserDetails={"shokisoftware@gmail.com":"pass1234","RiteshSing@gmail.com":"pass1234","AnirudhaGhatak@gmail.com":"pass1234"};
+var UserDetails=[
+                {emailid:"shokisoftware@gmail.com",password:"pass1234",name:"Shakhil"},
+                {emailid:"RiteshSing@gmail.com",password:"pass1234",name:"Ritesh"},
+                {emailid:"AnirudhaGhatak@gmail.com",password:"pass1234",name:"Aniruddha"}
+                ];
+
+var locations=[{displayName:"Kol-ITP"},{displayName:"Kol-BIPL"},{displayName:"Kol-TCPX"},{displayName:"Kol-GTP"}];
+
+var FoodVendors=[
+                {location:"Kol-ITP",rating:4.5,Id:1,Name:"A Mishra",Phone:"1234567890"},
+                {location:"Kol-BIPL",rating:2.1,Id:2,Name:"A Kumar",Phone:"1234567890"},
+                {location:"Kol-BIPL",rating:1.2,Id:3,Name:"S Sahoo",Phone:"1234567890"},
+                {location:"Kol-TCPX",rating:3.5,Id:4,Name:"D Paranjape",Phone:"1234567890"},
+                {location:"Kol-ITP",rating:3.1,Id:5,Name:"M Chaudhary",Phone:"1234567890"},
+                {location:"Kol-GTP",rating:4.8,Id:6,Name:"S Tendulkar",Phone:"1234567890"},
+                {location:"Kol-ITP",rating:1.5,Id:7,Name:"R Ashwin",Phone:"1234567890"},
+                {location:"Kol-GTP",rating:2.7,Id:8,Name:"S Dhawan",Phone:"1234567890"},
+                {location:"Kol-ITP",rating:1.9,Id:9,Name:"V Kohli",Phone:"1234567890"},
+                ];
+
+var FoodItems=[
+    {FoodItem:"Palak Paneer",Type:"Veg",Id:1,Price:"28 INR",quantity:1},
+    {FoodItem:"Mutton Roganjosh",Type:"NonVeg",Id:1,Price:"63 INR",quantity:5},
+    {FoodItem:"Chicken Masala",Type:"NonVeg",Id:2,Price:"58 INR",quantity:1},
+    {FoodItem:"Aloo Patol",Type:"Veg",Id:3,Price:"15 INR",quantity:2},
+    {FoodItem:"Aloo Posto",Type:"Veg",Id:2,Price:"23 INR",quantity:3},
+    {FoodItem:"Veg Thali",Type:"Veg",Id:2,Price:"45 INR",quantity:2},
+    {FoodItem:"Chicken Chowmein",Type:"NonVeg",Id:5,Price:"40 INR",quantity:5},
+    {FoodItem:"Egg Roll",Type:"NonVeg",Id:4,Price:"15 INR",quantity:5},
+    {FoodItem:"Daal Fry with Roti",Type:"Veg",Id:1,Price:"45 INR",quantity:2},
+    {FoodItem:"Fish Chop",Type:"NonVeg",Id:6,Price:"25 INR",quantity:4},
+    {FoodItem:"French Fries",Type:"Veg",Id:7,Price:"43 INR",quantity:3},
+    {FoodItem:"Chicken Momos",Type:"NonVeg",Id:5,Price:"22 INR",quantity:4},
+    {FoodItem:"Veg Momos",Type:"Veg",Id:8,Price:"18 INR",quantity:6},
+    {FoodItem:"Mutton Pasanda",Type:"NonVeg",Id:9,Price:"110 INR",quantity:4},
+    {FoodItem:"Ilish Mach",Type:"NonVeg",Id:9,Price:"90 INR",quantity:2},
+    {FoodItem:"Butter Naan",Type:"NonVeg",Id:8,Price:"45 INR",quantity:1},
+    {FoodItem:"Poori Sabji",Type:"Veg",Id:7,Price:"35 INR",quantity:2},
+    {FoodItem:"Chicken Biryani",Type:"NonVeg",Id:8,Price:"85 INR",quantity:5},
+    {FoodItem:"Moori Ghanto",Type:"NonVeg",Id:7,Price:"152 INR",quantity:10}
+]
